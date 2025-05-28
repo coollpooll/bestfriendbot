d07b0ce7c450995cc151a6677d46b242cf25fe53
coollpooll <coollpooll@proton.me>
2025-05-28 17:39:45 +0300

    main.py

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ async def telegram_webhook(req: Request):
     except Exception as e:
         await send_message(chat_id, f"⚠️ Ошибка: {str(e)}")
 
-    return {"ok": True"}
+    return {"ok": True}
 
 
 
